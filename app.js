8bc0b315079d6611a274f86c88454716b7e5b62f
elieldiniz <elielfantichelle@outlook.com.br>
2024-01-09 21:59:21 -0400

    iresposta json

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const app = express()
 // Configura uma rota para lidar com solicitações GET na raiz ("/")
 app.get('/', (req, res) => {
     // Envia uma resposta com um título HTML
-    res.send('<h1>Lista de tarefas</h1>')
+    res.send('<h1 class="name">eliel diniz Lista de tarefas</h1><br> <hr><h2>eliel diniz</h2>')
+})
+    //resposta json
+app.get('/json', (req, res) => {
+  
+    res.json({name:'eliel', idade:'25',})
 })
 
 // Inicia o servidor na porta 3000 e define uma função de retorno de chamada
